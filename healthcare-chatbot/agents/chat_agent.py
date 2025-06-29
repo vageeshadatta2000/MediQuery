2b3d92333b53287c5efdde7daafe809e130a5dc6
Vageesha Datta G <vageeshadattag@gmail.com>
2025-06-28 23:25:17 -0700

    Delete healthcare-chatbot directory

--- healthcare-chatbot/agents/chat_agent.py
+++
@@ -1,8 +0,0 @@
-from tools.rag_pipeline import get_rag_chain
-
-class MedicalChatAgent:
-    def __init__(self):
-        self.rag_chain = get_rag_chain()
-
-    def answer(self, query):
-        return self.rag_chain.run(query)
